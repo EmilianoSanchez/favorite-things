e928022c9183fd26bb01ab95fd6f95b08e1e42f7
Emiliano Sanchez <emi042003@gmail.com>
2019-08-10 14:33:52 -0300

    Validation of Metadata and Enum models

--- backend/restapi/models.py
+++ backend/restapi/models.py
@@ -13,21 +13,19 @@ class FavoriteThing(models.Model):
     title = models.CharField(max_length=200)
     description = models.TextField(blank=True, validators=[MinLengthValidator(10)])
     ranking = models.IntegerField(validators=[MinValueValidator(1)], default=1)
-    # metadata = models.TextField(blank=True, validators=[MinLengthValidator(10)])
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
     created_date = models.DateTimeField(auto_now_add=True)
     modified_date = models.DateTimeField(auto_now=True)
-    # audit_log =
 
     def __str__(self):
         return self.title
 
     def save(self, *args, **kwargs):
-        FavoriteThing.updateOtherRankings(ranking=self.ranking,category=self.category)
+        FavoriteThing.updateOtherRankings(ranking=self.ranking, category=self.category)
         super().save(*args, **kwargs)
 
     @staticmethod
-    def updateOtherRankings(ranking ,category ):
+    def updateOtherRankings(ranking, category):
         entry_set = FavoriteThing.objects.filter(ranking=ranking, category=category)
         if len(entry_set) >= 1:
             FavoriteThing.updateOtherRankings(ranking + 1, category)
@@ -37,12 +35,29 @@ class FavoriteThing(models.Model):
 
 class Enum(models.Model):
     name = models.CharField(max_length=100, unique=True)
-    values = models.TextField(unique=True, validators=[])
+    values = models.TextField()
 
 
 class Metadata(models.Model):
+    TEXT = 'Text'
+    DATE = 'Date'
+    NUMBER = 'Number'
+    ENUM = 'Enum'
+    TYPES = [
+        (TEXT, TEXT),
+        (DATE, DATE),
+        (NUMBER, NUMBER),
+        (ENUM, ENUM),
+    ]
+
     favorite_thing = models.ForeignKey(FavoriteThing, on_delete=models.CASCADE)
     key = models.CharField(max_length=100)
-    type = models.CharField(max_length=100)
+    type = models.CharField(max_length=6,
+                            choices=TYPES,
+                            default=TEXT,
+                            error_messages={'invalid_choice': f'Valid choices are {TEXT}, {DATE}, {NUMBER} and {ENUM}'})
     enum = models.ForeignKey(Enum, on_delete=models.CASCADE, null=True)
     value = models.TextField()
+
+    class Meta:
+        unique_together = ('favorite_thing', 'key')
--- backend/restapi/serializers.py
+++ backend/restapi/serializers.py
@@ -1,3 +1,6 @@
+import datetime
+import re
+
 from rest_framework import serializers
 
 from restapi.models import FavoriteThing, Category, Metadata, Enum
@@ -15,13 +18,68 @@ class FavoriteThingSerializer(serializers.HyperlinkedModelSerializer):
         fields = ['url', 'id', 'title', 'description', 'ranking', 'category', 'category_id', 'created_date', 'modified_date']
 
 
+# patternType = re.compile("^(Text|Date|Number|Enum)$")
+
+
+# def valid_types(value):
+#     if not patternType.match(value):
+#         raise serializers.ValidationError(
+#             'This field must be one of the following values: "Text", "Date", "Number" or "Enum"')
+
+
+def valid_date(date_text):
+    try:
+        datetime.datetime.strptime(date_text, '%Y-%m-%d')
+    except ValueError:
+        raise serializers.ValidationError({"value": "Incorrect date format. Should be YYYY-MM-DD."})
+
+
+def valid_number(value):
+    try:
+        float(value)
+    except ValueError:
+        raise serializers.ValidationError({"value": "Incorrect date format. Should be a number."})
+
+
+def valid_enum(enum, value):
+    if enum is None:
+        raise serializers.ValidationError({"enum": "When type field is Enum, enum field may not be blank."})
+    if value not in enum.values.split(","):
+        raise serializers.ValidationError({"value": f"Value must be one of {enum.name} enum values: {enum.values}"})
+
+
 class MetadataSerializer(serializers.HyperlinkedModelSerializer):
+    # type = serializers.CharField(validators=[valid_types])
     class Meta:
         model = Metadata
-        fields = ['url', 'id', 'key', 'type', 'enum', 'value', 'favorite_thing']
+        fields = ['url', 'id', 'key', 'type', 'enum', 'value', 'favorite_thing', 'favorite_thing_id']
+
+    def validate(self, data):
+        """
+        Validate Enum and Value fields according to Type field.
+        """
+        {
+            'Text': lambda data: None,
+            'Date': lambda data: valid_date(data['value']),
+            'Number': lambda data: valid_number(data['value']),
+            'Enum': lambda data: valid_enum(data["enum"], data['value']),
+        }[data['type']](data)
+        return data
+
+
+pattern = re.compile("^[a-zA-Z0-9]+(,[a-zA-Z0-9]+)*$")
+
+
+def list_of_strings(value):
+    # if not isinstance(value, str) or not pattern.match(value):
+    if not pattern.match(value):
+        raise serializers.ValidationError(
+            'This field must be an string with a comma separated list of values, such as "value1,value2,valueN".')
 
 
 class EnumSerializer(serializers.HyperlinkedModelSerializer):
+    values = serializers.CharField(validators=[list_of_strings])
+
     class Meta:
         model = Enum
         fields = ['url', 'id', 'name', 'values']
